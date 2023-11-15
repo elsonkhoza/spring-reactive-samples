446148893af630d9a4cb4a2f87d126a72932d096
elsoncom <145992011+elsoncom@users.noreply.github.com>
2023-11-15 18:48:04 +0200

    Spring webflux starter project

---
+++ spring-webflux-mongodb/src/test/java/com/elsonkhoza/webfluxmongodb/SpringWebfluxMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elsonkhoza.webfluxmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
