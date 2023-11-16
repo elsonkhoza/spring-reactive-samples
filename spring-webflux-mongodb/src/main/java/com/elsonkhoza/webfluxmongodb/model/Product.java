9d998a9b30d66bf8c4fc3ca66a4e92315b15f44f
elsoncom <145992011+elsoncom@users.noreply.github.com>
2023-11-16 12:39:36 +0200

    Product update

--- spring-webflux-mongodb/src/main/java/com/elsonkhoza/webfluxmongodb/model/Product.java
+++ spring-webflux-mongodb/src/main/java/com/elsonkhoza/webfluxmongodb/model/Product.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.Generated;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
@@ -12,8 +13,15 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @NoArgsConstructor
 @Document(collation = "products")
 public class Product {
+
+    // Will not be saved to the database if annotated with @Transient
+    @Transient
+    public static final String SEQUENCE_NAME = "products_sequence";
+
     @Id
     private Long id;
     private String name;
     private Double price;
+
+
 }
