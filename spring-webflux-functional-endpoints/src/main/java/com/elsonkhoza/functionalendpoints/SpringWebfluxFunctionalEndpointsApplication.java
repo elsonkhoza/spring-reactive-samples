c37c09c542b1a8127dc2f393302600a3d0ff0145
elsoncom <145992011+elsoncom@users.noreply.github.com>
2023-11-16 17:50:11 +0200

    Function endpoints project setup

---
+++ spring-webflux-functional-endpoints/src/main/java/com/elsonkhoza/functionalendpoints/SpringWebfluxFunctionalEndpointsApplication.java
@@ -0,0 +1,13 @@
+package com.elsonkhoza.functionalendpoints;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxFunctionalEndpointsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebfluxFunctionalEndpointsApplication.class, args);
+	}
+
+}
