e50b19547e08b3e01b293bbab15cb666b3c7b562
elsoncom <145992011+elsoncom@users.noreply.github.com>
2023-11-16 13:39:00 +0200

    Created ProductService and its methods

---
+++ spring-webflux-mongodb/src/main/java/com/elsonkhoza/webfluxmongodb/service/ProductService.java
@@ -0,0 +1,52 @@
+package com.elsonkhoza.webfluxmongodb.service;
+
+import com.elsonkhoza.webfluxmongodb.model.Product;
+import com.elsonkhoza.webfluxmongodb.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Range;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+    private final ProductRepository productRepository;
+    private final SequenceGeneratorService sequenceGeneratorService;
+
+    public Mono<Product> findProductByID(Long id) {
+        return productRepository.findById(id);
+    }
+
+    public Flux<Product> findAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Flux<Product> findProductOfPriceBetween(Double minPrice, Double maxPrice) {
+        return productRepository.findByPriceBetween(
+                Range.closed(minPrice, maxPrice)
+        );
+    }
+
+    public Mono<Product> updateProduct(Long id, Product requestProduct) {
+        return productRepository.findById(id)
+                .map(product -> {
+                    product.setName(requestProduct.getName());
+                    product.setPrice(requestProduct.getPrice());
+                    return product;
+                })
+                .flatMap(productRepository::save);
+    }
+
+    public Mono<Product> insertProduct(Product requestProduct) {
+        // Generate an id
+        requestProduct.setId(sequenceGeneratorService.getSequenceNumber(Product.SEQUENCE_NAME));
+        return productRepository.save(requestProduct);
+    }
+
+    public Mono<Void> deleteProduct(Long id) {
+        return productRepository.deleteById(id);
+    }
+
+
+}
