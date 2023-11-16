644ca519f3075d06d9f0d64475c877eb73168eb4
elsoncom <145992011+elsoncom@users.noreply.github.com>
2023-11-16 12:39:19 +0200

    Added mongodb sequence generator

---
+++ spring-webflux-mongodb/src/main/java/com/elsonkhoza/webfluxmongodb/model/DbSequence.java
@@ -0,0 +1,18 @@
+package com.elsonkhoza.webfluxmongodb.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collation = "db_sequences")
+public class DbSequence {
+
+    @Id
+    private String id;
+    private Long sequenceNumber;
+}
