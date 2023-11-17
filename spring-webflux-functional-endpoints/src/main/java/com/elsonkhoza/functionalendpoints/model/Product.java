3e960c16101af29cd3cb23508de10eb337392cbc
elsoncom <145992011+elsoncom@users.noreply.github.com>
2023-11-17 08:53:09 +0200

    Added product model and repository

---
+++ spring-webflux-functional-endpoints/src/main/java/com/elsonkhoza/functionalendpoints/model/Product.java
@@ -0,0 +1,26 @@
+package com.elsonkhoza.functionalendpoints.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "products")
+public class Product {
+
+    // Will not be saved to the database if annotated with @Transient
+    @Transient
+    public static final String SEQUENCE_NAME = "products_sequence";
+
+    @Id
+    private Long id;
+    private String name;
+    private Double price;
+
+
+}
---
+++ spring-webflux-functional-endpoints/src/main/java/com/elsonkhoza/functionalendpoints/repository/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.elsonkhoza.functionalendpoints.repository;
+
+
+import com.elsonkhoza.functionalendpoints.model.Product;
+import org.springframework.data.domain.Range;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
+
+@Repository
+public interface ProductRepository extends ReactiveMongoRepository<Product,Long> {
+    Flux<Product> findByPriceBetween(Range<Double> priceRange);
+}
