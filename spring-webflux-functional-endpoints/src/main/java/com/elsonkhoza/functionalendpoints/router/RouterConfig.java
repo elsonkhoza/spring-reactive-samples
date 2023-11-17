921b34d327e9f48d5fdb7bf49372a7e299e8af97
elsoncom <145992011+elsoncom@users.noreply.github.com>
2023-11-17 09:17:45 +0200

    Add post method for creating new product

---
+++ spring-webflux-functional-endpoints/src/main/java/com/elsonkhoza/functionalendpoints/router/RouterConfig.java
@@ -0,0 +1,23 @@
+package com.elsonkhoza.functionalendpoints.router;
+
+import com.elsonkhoza.functionalendpoints.handler.ProductHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+@Configuration
+@RequiredArgsConstructor
+public class RouterConfig {
+
+    private final ProductHandler productHandler;
+
+    @Bean
+    public RouterFunction<ServerResponse> routerFunction() {
+        return RouterFunctions.route()
+                .POST("router/",productHandler::createProduct)
+                .build();
+    }
+}
